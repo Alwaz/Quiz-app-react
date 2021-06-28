2a70b898ce54c611280d38bc7cf1853c039246e5
Alwaz <alwazkazi3@gmail.com>
2021-06-28 10:21:38 -0700

    defining states

--- src/Components/Questionare.js
+++ src/Components/Questionare.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import QuizArea from "./QuizArea";
 import ScoreArea from "./ScoreArea";
+import dataSet from "../api/dataSet";
 
 const Questionare = () => {
+  // define states
+  const [current, setCurrent] = useState(0);
+  const [dataSet, setDataSet] = useState([]);
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+
   return (
     <div>
       This is Questionare
